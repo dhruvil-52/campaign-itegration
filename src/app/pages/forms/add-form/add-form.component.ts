967d4299abad11d4db16e014d4f024e7cf0d4fde
Dhruvil <Dhruvil@buildesk.in>
2024-08-07 20:40:32 +0530

    fields mapping done for form data

--- src/app/pages/forms/add-form/add-form.component.ts
+++ src/app/pages/forms/add-form/add-form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { ControllerService } from 'src/app/shared/services/controller.service';
 import { environment } from 'src/environments/environment';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-add-form',
@@ -11,18 +12,43 @@ import { environment } from 'src/environments/environment';
 export class AddFormComponent implements OnInit {
   pages: any[] = [];
   forms: any[] = [];
-  formData: any = {};
+  formData: any = {
+    questions: [
+      {
+        "CRMKey": "firstname",
+        "CRMLabel": "First Name",
+        "IsRequired": true,
+        "FBKey": "",
+        "FBLabel": ""
+      },
+      {
+        "CRMKey": "lastname",
+        "CRMLabel": "Last Name",
+        "FBKey": "",
+        "FBLabel": ""
+      },
+      {
+        "CRMKey": "mobile",
+        "CRMLabel": "Mobile",
+        "IsRequired": true,
+        "FBKey": "",
+        "FBLabel": ""
+      },
+      {
+        "CRMKey": "email",
+        "CRMLabel": "Email",
+        "FBKey": "",
+        "FBLabel": ""
+      }
+    ]
+  };
+  questions: any = [];
   loggedInUserDetails: any = {};
-  requiredFields: any = [
-    { RequiredField: "First Name*" },
-    { RequiredField: "Last Name" },
-    { RequiredField: "Phone*" },
-    { RequiredField: "Email" }
-  ];
 
   constructor(
     public dialogRef: MatDialogRef<AddFormComponent>,
-    private cs: ControllerService) {
+    private cs: ControllerService,
+    private ts: ToastrService) {
   }
 
   ngOnInit(): void {
@@ -58,16 +84,38 @@ export class AddFormComponent implements OnInit {
   onFormChange() {
     console.log(this.formData.Form)
     this.cs.getFormDataByFormId(this.formData.Form, this.formData.Page).then((response: any) => {
-      this.formData.questions = response.questions;
-      console.log("Form Data", JSON.stringify(this.formData));
+      this.questions = response.questions;
+      console.log("Form Data", JSON.stringify(this.questions));
     }).catch((e) => {
       console.log("Error while getting Form Details By form Id", e)
     })
   }
 
+  onFieldSelected(event: any, field: any) {
+    let data = JSON.parse(JSON.stringify(event.value));
+    field.FBKey = data.key
+    field.FBLabel = data.label
+  }
+
+  checkRequiredQuestionFieldsAreEmpty() {
+    return this.formData.questions.some((e: any) => e.IsRequired && !e.FBKey)
+  }
+
   onSave(): void {
     console.log(JSON.stringify(this.formData))
-    this.dialogRef.close({ data: this.formData });
+    if (!this.formData.name) {
+      this.ts.error("Please Enter Name");
+    } else if (!this.formData.Page) {
+      this.ts.error("Please select Page");
+    } else if (!this.formData.Form) {
+      this.ts.error("Please select Form");
+    } else if (!this.formData.Form) {
+      this.ts.error("Please select Form");
+    } else if (this.checkRequiredQuestionFieldsAreEmpty()) {
+      this.ts.error("Please Fill required Form fields");
+    } else {
+      this.dialogRef.close({ data: this.formData });
+    }
   }
 
   onCancel(): void {
